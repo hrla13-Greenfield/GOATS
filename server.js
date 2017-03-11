d7ba56f9e97b4e6c172c3a404cfbc73e8386ad76
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-03-10 21:37:28 -0800

    resolved conflicts

--- server.js
+++ server.js
@@ -1,2 +1,20 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+const myRouter = require(`${__dirname}/server/router.js`);
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
+app.use(express.static('./output'));
+app.use('/api', myRouter);
+
+app.listen(port, () => {
+  console.log('connected to port 8000');
+});
+
+
+module.exports = app;
---
+++ server/router.js
@@ -0,0 +1,18 @@
+const myRouter = require('express').Router();
+
+myRouter.route('/')
+  .get((req, res) => {
+
+  })
+  .post((req, res) => {
+
+  });
+
+myRouter.route('/users')
+  .get((req, res) => {
+
+  })
+  .post((req, res) => {
+
+  });
+
