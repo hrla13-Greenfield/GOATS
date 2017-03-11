7d4d70c64d216d47337ed3d1c9749bc846ac2a3b
Alexandre Kim <alexandrekim93@gmail.com>
2017-03-11 11:30:55 -0800

    skeleton

--- api/router.js
+++ api/router.js
@@ -1,11 +1,11 @@
 const myRouter = require('express').Router();
+const controller = require('./controller');
 
 myRouter.route('/')
   .get((req, res) => {
 
   })
   .post((req, res) => {
-
   });
 
 myRouter.route('/users')
@@ -13,6 +13,8 @@ myRouter.route('/users')
 
   })
   .post((req, res) => {
+    controller.createUser(req, res)
 
   });
 
+module.exports = myRouter;
--- server.js
+++ server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
-const myRouter = require(`${__dirname}/server/router.js`);
+const myRouter = require(`${__dirname}/api/router.js`);
 const app = express();
 
-const port = 8000;
+const PORT = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 app.use(express.static('./output'));
 app.use('/api', myRouter);
 
-app.listen(port, () => {
-  console.log('connected to port 8000');
+app.listen(PORT, () => {
+  console.log('connected to ' + PORT);
 });
 
 
