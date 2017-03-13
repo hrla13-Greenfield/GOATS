09a1494c38bb0972c827bdb3bc334d844b258932
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-03-13 13:02:42 -0700

    fixed

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers/index.js';
 
+
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
    <App />
