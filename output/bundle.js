c8fef2d8704a25cd4261c610ec5631669489ceb2
Sandra Jay <henkel.sandra1@gmail.com>
2017-03-14 12:34:46 -0700

    "Api calls for survey/tree completed"

--- output/bundle.js
+++ output/bundle.js
@@ -8304,21 +8304,73 @@ function getData(selection) {
       distance: selection.distance }]
   };
 }
-
 function selectChoice(option) {
   if (option === 'food') {
     return {
       type: 'CHOICES_SELECTED',
-      payload: [{ img: 'http://www.maturetimes.co.uk/wp-content/uploads/2013/10/breakfast-including-coffee-honey-848x478.jpg', option: 'breakfast' }, { img: 'https://cpplymouth.com/up/Creekside_event_table_setting_with_salmon_dinner_and_wine.jpg', option: 'lunch/dinner' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'any' }]
+      payload: [{ img: 'http://www.maturetimes.co.uk/wp-content/uploads/2013/10/breakfast-including-coffee-honey-848x478.jpg', option: 'breakfast' }, { img: 'https://cpplymouth.com/up/Creekside_event_table_setting_with_salmon_dinner_and_wine.jpg', option: 'lunch/dinner' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'anyfood' }]
     };
   } else if (option === 'party') {
     return {
       type: 'CHOICES_SELECTED',
-      payload: [{ img: 'https://i.ytimg.com/vi/CX8Uuynkppw/maxresdefault.jpg', option: 'bar' }, { img: 'https://s-media-cache-ak0.pinimg.com/originals/42/26/8a/42268a15745703bbff25a4ca2e7e461b.jpg', option: 'club' }, { img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTpwzRlQlQemkHCfO4JA4yYbGX_tnStjZLPxRnaMaIcjTeGq0N', option: 'lounge' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'any' }]
+      payload: [{ img: 'https://i.ytimg.com/vi/CX8Uuynkppw/maxresdefault.jpg', option: 'bar' }, { img: 'https://s-media-cache-ak0.pinimg.com/originals/42/26/8a/42268a15745703bbff25a4ca2e7e461b.jpg', option: 'club' }, { img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTpwzRlQlQemkHCfO4JA4yYbGX_tnStjZLPxRnaMaIcjTeGq0N', option: 'lounge' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'anyparty' }]
+    };
+  } else if (option === 'sports') {
+    return {
+      type: 'CHOICES_SELECTED',
+      payload: [{ img: 'https://indonesia.tripcanvas.co/bali/wp-content/uploads/2015/02/kitesurfing21.jpg', option: 'extreme' }, { img: 'http://caffeyoga.com/wp-content/uploads/2016/01/beach-yoga1.jpg', option: 'chill' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'anysports' }]
+    };
+  } else if (option === 'relax') {
+    return {
+      type: 'CHOICES_SELECTED',
+      payload: [{ img: 'http://bhubaneswarlive.com/wp-content/uploads/2017/01/spa-services-in-bhubaneswar.jpg', option: 'body&mind' }, { img: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQlBVJ8BIMYMBvtYycyIWcH4fQ-GLJ2eal2tLfDcYWfTQiaPURd3w', option: 'getOut' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'anyrelax' }]
+    };
+  } else if (option === 'activity') {
+    return {
+      type: 'CHOICES_SELECTED',
+      payload: [{ img: 'http://www.visitcalifornia.com/sites/default/files/styles/welcome_image/public/VCW_D_SantaCruz_T2_Lisa_7547806186_6171c42b54_o_1280x642.jpg', option: 'entertainment' }, { img: 'http://www.travelinsurance.org/wp-content/uploads/2009/11/skydiving.jpg', option: 'daredevil' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'anyactivity' }]
+    };
+    //server calls
+  } else if (option === 'breakfast') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'food', filter: 'coffee', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'lunch/dinner') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'food', filter: 'restaurants', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
     };
   } else if (option === 'bar') {
     return function (dispatch) {
-      _axios2.default.get('api/getBars').then(function (results) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'bars', filter: 'bars', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
         data = results.data.businesses;
         selection = data[Math.floor(Math.random() * data.length)];
         dispatch(getData(selection));
@@ -8329,7 +8381,12 @@ function selectChoice(option) {
     };
   } else if (option === 'club') {
     return function (dispatch) {
-      _axios2.default.get('api/getClubs').then(function (results) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'clubs', filter: 'danceclubs', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
         data = results.data.businesses;
         selection = data[Math.floor(Math.random() * data.length)];
         dispatch(getData(selection));
@@ -8342,6 +8399,198 @@ function selectChoice(option) {
     return function (dispatch) {
       _axios2.default.get('api/getActivities', { params: {
           term: 'lounge', filter: 'lounges', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'extreme') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'sports', filter: 'boxing,bootcamps,martialarts,flyboarding,hanggliding,horseracing,mountainbiking,rafting,rock_climbing,kiteboarding,diving', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'chill') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'fitness', filter: 'tennis,golf,yoga,gyms,hiking,horsebackriding,pilates', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'body&mind') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'beauty', filter: 'beautysvc', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'getOut') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'active', filter: 'farms,parks,beaches,shoppingcenters', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'entertainment') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'arts', filter: 'museums,opera,theater,wineries,winetastingrooms,galleries,movietheaters,zoos,amusementparks,fleamarkets,zoos,planetarium, festivals', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'daredevil') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'active', filter: 'bungeejumping,hot_air_balloons,jetskis,ziplining,gokarts,paintball', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'any') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: '', filter: 'bungeejumping,hot_air_balloons,jetskis,ziplining,gokarts,paintball,museums,opera,theater,wineries,winetastingrooms,galleries,movietheaters,zoos,amusementparks,fleamarkets,zoos,planetarium,farms,parks,beaches,shoppingcenters,beautysvc,tennis,golf,yoga,gyms,hiking,horsebackriding,pilates,boxing,bootcamps,martialarts,flyboarding,hanggliding,horseracing,mountainbiking,rafting,rock_climbing,kiteboarding,diving,launches,danceclubs,bars,festivals', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'anyfood') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'food', filter: 'restaurants', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'anyparty') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'clubs', filter: 'nightlife', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'anysports') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'fitness', filter: 'fitness', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'anyrelax') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'spa', filter: 'beautysvc', zip: '90024' },
+        headers: {
+          'Content-type': 'application/x-www-form-urlencoded'
+        }
+      }).then(function (results) {
+        data = results.data.businesses;
+        selection = data[Math.floor(Math.random() * data.length)];
+        dispatch(getData(selection));
+        console.log('results.data.businesses', results.data.businesses);
+      }).catch(function (err) {
+        console.error(err);
+      });
+    };
+  } else if (option === 'anyactivity') {
+    return function (dispatch) {
+      _axios2.default.get('api/getActivities', { params: {
+          term: 'entertainment', filter: 'arts', zip: '90024' },
         headers: {
           'Content-type': 'application/x-www-form-urlencoded'
         }
@@ -31595,7 +31844,7 @@ exports.default = function () {
 var initialState = {
   finalSelection: false,
 
-  firstLoad: [{ img: 'http://ifthedevilhadmenopause.com/wp-content/uploads/2014/08/sri-santrupti-restaurant-21346657591-1.png', option: 'food' }, { img: 'http://www.miamiandbeaches.com/~/media/Images/GMCVB/MiamiAndBeaches/Featured%20Articles/Featured%20Article%20Headers/clubs-101-3-612-x-338.jpg', option: 'party' }, { img: 'http://www.pkvitality.com/wp-content/uploads/2016/12/accuracy.jpg', option: 'sports' }, { img: 'http://s.hswstatic.com/gif/relaxation-quiz-orig.jpg', otpion: 'relax' }, { img: 'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/gallery/destinations/disneyland-park/disneyland-gallery25.jpg?21042014145156', option: 'activity' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'any' }]
+  firstLoad: [{ img: 'http://ifthedevilhadmenopause.com/wp-content/uploads/2014/08/sri-santrupti-restaurant-21346657591-1.png', option: 'food' }, { img: 'http://www.miamiandbeaches.com/~/media/Images/GMCVB/MiamiAndBeaches/Featured%20Articles/Featured%20Article%20Headers/clubs-101-3-612-x-338.jpg', option: 'party' }, { img: 'http://www.pkvitality.com/wp-content/uploads/2016/12/accuracy.jpg', option: 'sports' }, { img: 'http://s.hswstatic.com/gif/relaxation-quiz-orig.jpg', option: 'relax' }, { img: 'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/gallery/destinations/disneyland-park/disneyland-gallery25.jpg?21042014145156', option: 'activity' }, { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'any' }]
 };
 
 //console.log('in redcucer')
--- src/reducers/choices.jsx
+++ src/reducers/choices.jsx
@@ -8,10 +8,10 @@ var initialState = {
     { img: 'http://ifthedevilhadmenopause.com/wp-content/uploads/2014/08/sri-santrupti-restaurant-21346657591-1.png', option: 'food' },
     { img: 'http://www.miamiandbeaches.com/~/media/Images/GMCVB/MiamiAndBeaches/Featured%20Articles/Featured%20Article%20Headers/clubs-101-3-612-x-338.jpg', option: 'party' },
     { img: 'http://www.pkvitality.com/wp-content/uploads/2016/12/accuracy.jpg', option: 'sports' },
-    { img: 'http://s.hswstatic.com/gif/relaxation-quiz-orig.jpg', otpion: 'relax' },
-    { img: 'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/gallery/destinations/disneyland-park/disneyland-gallery25.jpg?21042014145156', option: 'activity'},
+    { img: 'http://s.hswstatic.com/gif/relaxation-quiz-orig.jpg', option: 'relax' },
+    { img: 'https://secure.parksandresorts.wdpromedia.com/resize/mwImage/1/640/360/75/wdpromedia.disney.go.com/media/wdpro-assets/dlr/gallery/destinations/disneyland-park/disneyland-gallery25.jpg?21042014145156', option: 'activity' },
     { img: 'https://abtasty-mtgy74j.netdna-ssl.com/content/uploads/homer-brain-monkey.jpg', option: 'any' },
-  ]
+  ],
 };
 
   //console.log('in redcucer')
@@ -30,5 +30,6 @@ export default function(state = initialState, action) {
   }
   return state;
 }
+
 //main purpose of reducers is to update state
 
