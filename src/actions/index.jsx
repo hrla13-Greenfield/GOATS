366c26ba3129d1d67b1b05f196efdbebb81ec0ea
Sandra Jay <henkel.sandra1@gmail.com>
2017-03-13 18:17:07 -0700

    "Async yelp call issue wip"

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,3 +1,7 @@
+
+import axios from 'axios';
+var data;
+
 export function selectChoice(option) {
   console.log('in action creator');
   //here I would make axios get to db
@@ -15,14 +19,22 @@ export function selectChoice(option) {
                { img: 'https://s-media-cache-ak0.pinimg.com/originals/42/26/8a/42268a15745703bbff25a4ca2e7e461b.jpg', option: 'club' }, 
                ]
   };
- } else if (option === 'bar'){
+ } else if (option === 'bar') {
+   axios.get('api/getBars')
+   .then( results => {
+     data = results.data.businesses[0];
+     console.log('results.data.businesses', results.data.businesses);
+   })
+   .catch(err => {
+     console.error(err)
+   })
    return {
      type: 'FINAL_SELECTED',
-     payload: [{ name: 'this works', option: 'none'}, 
-               { activity: 'dance', option: 'none' },
-              ],
+     payload: [{ name: data.name, activity: 'dance'}, 
+              ]
    };
  }
+
 
 //axios calls go in here NOT in the reducers
 
