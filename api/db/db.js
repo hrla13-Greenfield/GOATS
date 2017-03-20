800adcbb03364527f0f54abc9cdf82cfd6e16c31
Brandon Kleiman <brandonwkleiman@gmail.com>
2017-03-20 09:37:37 -0700

    fixed typo

--- api/db/db.js
+++ api/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('mysql://bb23c85be92ed8:fac8d99a@us-cdbr-iron-east-03.cleardb.net/heroku_89dc2ee2c6c627c?reconnect=true, {
+const db = new Sequelize('mysql://bb23c85be92ed8:fac8d99a@us-cdbr-iron-east-03.cleardb.net/heroku_89dc2ee2c6c627c?reconnect=true', {
   host: 'localhost',
   port: 3306,
   timestamps: false,
